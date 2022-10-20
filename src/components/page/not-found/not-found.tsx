8fc4d8130a8f7091437edfd96646d3101d1130c4
Alexander Khapchenko <alexanderfox1337@gmail.com>
2022-10-20 18:19:47 +0300

    Updated not found page

--- src/components/page/not-found/not-found.tsx
+++ src/components/page/not-found/not-found.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Box, Button, Card, CardContent, Typography } from '@mui/material';
+
+import { AppRoute } from '../../../common/enums';
+import styles from './styles.module.scss';
 
 const NotFound: React.FC = () => {
-  return <h1>Not Found Page</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <Box className={styles.box}>
+      <Card className={styles.card}>
+        <CardContent className={styles.cardContent}>
+          <Typography variant="h5" color="text.secondary">
+            Page Not Found
+          </Typography>
+          <Button
+            variant="contained"
+            onClick={(): void => navigate(AppRoute.ROOT)}
+          >
+            Go to main page
+          </Button>
+        </CardContent>
+      </Card>
+    </Box>
+  );
 };
 
 export { NotFound };
